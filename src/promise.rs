cf833cbc3ec66a558958a2fc6b1a2e9a62b27978
Delan Azabani <delan@azabani.com>
2020-05-31 18:26:40 +1000

    update Promise variants to align with OpenBSD 6.7

--- src/promise.rs
+++ src/promise.rs
@@ -2,112 +2,112 @@
 // using the helper documented at </variants/README.md>.
 
 pub enum Promise {
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Audio,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Bpf,
 
-    /// OpenBSD 6.0–6.6
+    /// OpenBSD 6.0–6.7
     Chown,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Cpath,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Disklabel,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Dns,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Dpath,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Drm,
 
-    /// OpenBSD 6.3–6.6
+    /// OpenBSD 6.3–6.7
     Error,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Exec,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Fattr,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Flock,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Getpw,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Id,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Inet,
 
     /// OpenBSD 5.9–6.0
     Ioctl,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Mcast,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Pf,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Proc,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     ProtExec,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Ps,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Recvfd,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Route,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Rpath,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Sendfd,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Settime,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Stdio,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Tape,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Tmppath,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Tty,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Unix,
 
-    /// OpenBSD 6.4–6.6
+    /// OpenBSD 6.4–6.7
     Unveil,
 
-    /// OpenBSD 6.5–6.6
+    /// OpenBSD 6.5–6.7
     Video,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Vminfo,
 
-    /// OpenBSD 6.1–6.6
+    /// OpenBSD 6.1–6.7
     Vmm,
 
-    /// OpenBSD 5.9–6.6
+    /// OpenBSD 5.9–6.7
     Wpath,
 
     // FIXME rust-lang/rust#44109
